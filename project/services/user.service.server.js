bade97115abf6be53a2b6e664a877290b2b7d845
seshasai1@ccs.neu.edu <srivatsav@Seshas-MacBook-Pro.local>
2017-11-10 14:44:54 -0500

    Fixed google authentication

--- project/services/user.service.server.js
+++ project/services/user.service.server.js
@@ -6,18 +6,18 @@ var bcrypt = require("bcrypt-nodejs");
 
 // Running Locally
 
-// var googleConfig = {
-//     clientID     : "19483974661-f0g4hn8mplkvlc9dm9kip4refl4k0u2i.apps.googleusercontent.com",
-//     clientSecret : "jmupSkxQtUqRuuhAKTcOfddz",
-//     callbackURL  : "http://127.0.0.1:3000/auth/google/callback"
-// };
+var googleConfig = {
+    clientID     : "19483974661-f0g4hn8mplkvlc9dm9kip4refl4k0u2i.apps.googleusercontent.com",
+    clientSecret : "jmupSkxQtUqRuuhAKTcOfddz",
+    callbackURL  : "http://127.0.0.1:3000/auth/google/callback"
+};
 
 // Running on a host
-var googleConfig = {
-    clientID     : process.env.GOOGLE_CLIENT_ID,
-    clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL  : process.env.GOOGLE_CALLBACK_URL
-};
+// var googleConfig = {
+//     clientID     : process.env.GOOGLE_CLIENT_ID,
+//     clientSecret : process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL  : process.env.GOOGLE_CALLBACK_URL
+// };
 
 
 
@@ -40,8 +40,8 @@ module.exports= function(app, models){
     app.get('/api/project/findallusers', findallusers);
     app.get('/auth/google/callback',
         passport.authenticate('google', {
-            successRedirect: '/project/#/profile',
-            failureRedirect: '/project/#/login'
+            successRedirect: '/#/profile',
+            failureRedirect: '/#/login'
         }));
     
   passport.use('FilmNerd', new LocalStrategy(localStrategy));
